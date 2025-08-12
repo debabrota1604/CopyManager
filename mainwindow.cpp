35f6948c06d1129c06357eca8725ebe5f14ac18a
Debabrota Sadhukhan <debabrota.sadhukhan3@gmail.com>
2025-08-12 16:51:47 +0530

    Button and textview added with file path picker

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,4 +11,18 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_btnSelectSource_clicked()
+{
+    QString filePath = QFileDialog::getOpenFileName(
+        this,
+        "Select Source File",
+        QString(),                  // Start in default dir
+        "All Files (*.*)"           // File filter
+        );
+
+    if (!filePath.isEmpty()) {
+        ui->txtFilePath->setText(filePath);
+    }
 }
